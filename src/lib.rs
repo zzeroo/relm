3a3ab92b7dae98074ede91256ee43034d70f3061
Antoni Boucher <bouanto@zoho.com>
2017-05-19 22:06:50 -0400

    Add IntoPair trait to allow returning Inhibit when a pair is needed

--- src/lib.rs
+++ src/lib.rs
@@ -90,8 +90,8 @@ extern crate relm_core;
 
 mod component;
 mod container;
+mod into;
 mod macros;
-mod option;
 mod stream;
 mod widget;
 
@@ -116,7 +116,7 @@ pub use relm_core::EventStream;
 
 pub use container::{Container, ContainerWidget, RelmContainer};
 pub use component::Component;
-pub use option::IntoOption;
+pub use into::{IntoOption, IntoPair};
 use stream::ToStream;
 pub use widget::Widget;
 
